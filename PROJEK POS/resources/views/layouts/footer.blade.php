c8522b86667b7492139174d509c93cbef90d731a
OliviaRamadhani <oliviamdhn@gmail.com>
2024-09-11 06:37:53 +0700

    POS

---
+++ PROJEK POS/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>AON Cashier © {{ date('Y') }} || Edited by <strong><a target="_blank" href="https://fahimanzam.netlify.app">AODN</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
